e89ac140223aed1f40c1e8e12ac3cf3337b24abb
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-30 14:30:03 +0530

    30janp

---
+++ controlstat.py
@@ -0,0 +1,56 @@
+# conditional statment:-
+# if, if-else, if-elif-else
+
+            # if statment:
+                # syntax:-if(condition):
+                    # statment---1
+                    # statment---2
+
+            # if else statment:
+                # syn:-if (condition):
+                    # statment---1
+
+                        # else (condition):
+                            # statment---1
+
+# x=int(input("Enter any number :"))
+# if x%2==0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+    # output:even=Even Number
+    # output:odd=Odd Number
+
+# x=int(input("Enter 1st number:"))
+# y=int(input("Enter 2nd number:"))
+# if x>y:
+#     print(f" is greater")
+# else:
+#     print(f"{y} is greater")
+
+
+# x=int(input("Enter your age:"))
+# if x>=18:
+#     print("You can vote")
+# else:
+#     print("You can't vote")
+   
+    # output:You can vote
+    # output:You can't vote
+                # ---------------------multiple if-ifs---------------------
+# if (condition1 and condition2 and condition3):
+#     statment---
+        # if (condition):
+                #    if (condition):
+                      #    if (condition):
+# ---------------------------------------------------------------------
+
+
+
+
+
+# intrative statment:-
+# while, for
+
+# Transfer statment:-
+# break, continue, pass
