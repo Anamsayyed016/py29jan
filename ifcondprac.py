5ae753cd4629f77525c08b488593b6acd790669a
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-30 16:49:06 +0530

    30prac

---
+++ ifcondprac.py
@@ -0,0 +1,15 @@
+x=int(input("Enter 1st number:"))
+y=int(input("Enter 2nd number:"))
+z=int(input("Enter 3rd number:"))
+
+if x>y:
+    print(f"{x} is greater")
+
+if y>z:
+    print(f"{y} is greater")
+
+if z>x:
+    print(f"{z} is greater")
+
+    
+    
