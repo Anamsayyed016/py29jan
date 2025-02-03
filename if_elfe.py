48989e45f6ceb9d64dd2239c4c791a75fb652e0b
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-03 18:25:49 +0530

    3rdfebpy

--- if_elfe.py
+++ if_elfe.py
@@ -5,29 +5,29 @@
 
 # elif(condition):
 
-x=int(input("Enter 1st number:"))
-y=int(input("Enter 2nd number:"))
-z=int(input("Enter 3rd number:"))
+# x=int(input("Enter 1st number:"))
+# y=int(input("Enter 2nd number:"))
+# z=int(input("Enter 3rd number:"))
 
-if (x>y and x>z):
-    print(f"{x} is greater")
+# if (x>y and x>z):
+#     print(f"{x} is greater")
 
-elif(y>z and y>x):
-    print(f"{y} is greater")
+# elif(y>z and y>x):
+#     print(f"{y} is greater")
 
-elif(z>x and z>y):
-    print(f"{z} is greater")
+# elif(z>x and z>y):
+#     print(f"{z} is greater")
 
-elif(x==y):
-    print(f"{x} and {y} are equal")
+# elif(x==y):
+#     print(f"{x} and {y} are equal")
 
-elif(y==z):
-    print(f"{y} and {z} are equal")
+# elif(y==z):
+#     print(f"{y} and {z} are equal")
 
-elif(z==x):
-    print(f"{z} and {x} are equal")
+# elif(z==x):
+#     print(f"{z} and {x} are equal")
 
-else:
-    print("please enter valid number")
+# else:
+#     print("please enter valid number")
 
     
---
+++ while_for.py
@@ -0,0 +1,21 @@
+# while-loop---for infinite
+# for-loop---not use for infinite
+# n natural number:
+# n=int(input("enter any no. "))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+# ------------------------------------------    
+
+# even number:
+n=int(input("enter any no. "))
+i=1
+while (i<=n):
+   x=2*i
+print(f'2*{i}={x}')
+print(x)
+i+=1
+
+print("after break",i,n)
