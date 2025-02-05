8a7e97914ffc8e2a59ac5f64f1d9b9dd2036a3bd
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-05 14:31:16 +0530

    5febp

---
+++ for.py
@@ -0,0 +1,52 @@
+# syn:-
+# for i an collection:
+                    # string, list, tuple, dict.
+                        #print
+
+# n=(input("enter your name:"))
+# # print(type(n))
+# for i in n:
+#     print(i)
+# # output:a
+        # n
+        # a
+        # m
+
+# n='jklm'
+# n=(input("enter any string:"))
+# for i in n:
+#     x=ord(i)+5
+
+#     y= chr(x)
+    # print(y,end='')
+
+
+# Range:
+# genrate the collection
+# range(start,stop,step)
+
+# n=range(1,11)
+# print(list(n))
+# print(tuple(n))
+
+
+# n=range(-1,-11,-1)
+# print(list(n))
+# print(tuple(n))
+
+# x=range(-10,0)
+# print(list(x))
+# print(tuple(x))
+
+# n=int(input("enter any value:"))
+# x=range(2,n+1,2)
+# print(list(x))
+
+# n=int(input("enter any value:"))
+# x=range(1,n+1,2)
+# print(list(x))
+
+# l=10
+# x=range(l-1,-1,-1)
+# print(list(x))
+
