92ef4e96ed268a4043cf89c3e8e2db13f90e829c
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-06 14:30:29 +0530

    6febpy

---
+++ eval.py
@@ -0,0 +1,13 @@
+x = eval(input("Enter any value :" ))
+print(x)
+print(type(x))
+
+# output-anam
+# <class 'str'>
+
+# output-(10, 20, 30)
+# <class 'tuple'>
+
+# output-[10, 20, 30]
+# <class 'list'>
+
---
+++ transstat.py
@@ -0,0 +1,62 @@
+# breal=--  exit from loop
+# continue---- skip current itreation
+# pass ---- skip current block
+
+# BREAK:-------------------------------
+# n = int(input("enter your no :"))
+# i = 1
+# while i < n:
+#     # Compare i with 6
+#     if i == 6: 
+#         break
+#     print(i)
+#     i = i + 1
+# print("hello")
+
+# Continue:---------------------------
+
+# n = int(input("enter your no :"))
+# i = 1
+# while i < n:
+#     # Compare i with 6
+#     if i == 6: 
+#         i=i+1
+#         # print(i)
+#         continue
+#     print(i)
+#     i = i + 1
+# print("hello")
+# output  =   1
+            # 2
+            # 3
+            # 4
+            # 5
+            # 7
+            # 8
+            # 9
+
+    
+    # PASS:--------------------
+
+n = int(input("enter your no :"))
+i = 1
+while i < n:
+    # Compare i with 6
+    if i == 6: 
+        # print(i)
+        pass
+    print(i)
+    i = i + 1
+    #output:  1
+            # 2
+            # 3
+            # 4
+            # 5
+            # 6
+            # 7
+            # 8
+            # 9
+            # 10
+
+# --------------------------------
+
