f99d597cbe1b8891bf6ee1e8076187dbaef96fee
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-12 14:29:20 +0530

    12febpy

--- function.py
+++ function.py
@@ -18,7 +18,7 @@
 # print(y,x)
 # print(p)
 # -------------------------------------------------------
-# 1- positional arguement:-
+# 1- Positional arguement:-
 
 # def fun_name(x,y):
 #     return x+y
@@ -27,15 +27,60 @@
 # print(z)
 # output=9
 
-# keyword Argument:-
-def fun_name(x,y):
+# Keyword Argument:-
+# def fun_name(x,y):
    
-    print("value of x",x)
-    print("value of y",y)
-    return x+y
-p=int(input("Enter 1st value :"))
-q=int(input("Enter 2nd  value :"))
-z=fun_name(y=p,x=q)
-print(z)
-print (fun_name.__doc__)
-print(dir(fun_name))
+#     print("value of x",x)
+#     print("value of y",y)
+#     return x+y
+# p=int(input("Enter 1st value :"))
+# q=int(input("Enter 2nd  value :"))
+# z=fun_name(y=p,x=q)
+# print(z)
+# print (fun_name.__doc__)
+# print(dir(fun_name))
+
+# -----------------------------------------------------------------------
+        #Default Argument:-
+
+# def fun_name(x=0,y=0,z=0):
+#     # p=x+y+z
+#     # print("Hi")
+#     print('x = ',x)
+#     print('y = ',y)
+#     print('z = ',z)
+
+#     return x+y+z
+# x=fun_name(4,5,6)
+# # x=fun_name(4,5,6,7)
+
+# # print(fun_name(1,2,3))
+# # x=fun_name(5)
+
+# print(x)
+
+# ---------------------------------------------------------------------------------
+
+# Variable Length Argument(* args[single star---all argument is not fix and asign with any name])
+
+# def add (*n):
+#     print(n)
+#     print(type(n))
+#     sum=0
+#     for i in n:
+#         sum=sum+1
+#     return sum
+# n=add(2,4,5,6)
+# print(n)
+    
+# def add (*n):
+#     print(n)
+#     print(type(n))
+#     sum=0
+#     for i in n:
+#         for x in i:
+#             sum=sum+x
+#     return sum
+# p=eval(input("Enter any tuple :"))
+# n=add(p)
+# print(n)
